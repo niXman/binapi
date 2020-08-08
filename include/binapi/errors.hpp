25d0c97e21e88f7de4306f9ed50571d8e757f90d
niXman <i.nixman@autistici.org>
2020-08-08 15:55:45 +0300

    licenses was updated

--- include/binapi/errors.hpp
+++ include/binapi/errors.hpp
@@ -1,4 +1,14 @@
 
+// ----------------------------------------------------------------------------
+//                              Apache License
+//                        Version 2.0, January 2004
+//                     http://www.apache.org/licenses/
+//
+// This file is part of binapi(https://github.com/niXman/binapi) project.
+//
+// Copyright (c) 2019-2020 niXman (github dot nixman dog pm.me). All rights reserved.
+// ----------------------------------------------------------------------------
+
 #ifndef __binapi__errors_hpp
 #define __binapi__errors_hpp
 
