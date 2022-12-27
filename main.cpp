d2a6e6811b7b207e59c4bbcee43cf6ef70f35152
Patrick Kelleher <spk.kelleher@gmail.com>
2022-12-27 11:21:08 -0500

    Included example of new functions

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@
 #include "bg_api/websocket.hpp"
 #include "bg_api/api.hpp"
 #include "bg_api/flatjson.hpp"
+#include "bg_api/simdjson.h"
 
 #include <boost/asio/io_context.hpp>
 #include <boost/asio/steady_timer.hpp>
@@ -28,8 +29,8 @@ std::string read_file(const char *fname) {
     assert(file);
 
     std::string res{
-         (std::istreambuf_iterator<char>(file))
-        ,(std::istreambuf_iterator<char>())
+        (std::istreambuf_iterator<char>(file)),
+        (std::istreambuf_iterator<char>())
     };
 
     return res;
@@ -53,26 +54,57 @@ std::string read_file(const char *fname) {
 /*************************************************************************************************/
 
 int main(int argc, char **argv) {
-    assert(argc == 3);
-    const std::string pk = argv[1];
-    const std::string passphrase = argv[2];
-    const std::string sk = argv[3];
+    //assert(argc == 3);
+    //const std::string pk = argv[1];
+    //const std::string passphrase = argv[2];
+    //const std::string sk = argv[3];
 
     std::cout.precision(15);
 
     boost::asio::io_context ioctx;
-    bg_api::ws::websockets wsp(ioctx, "ws.bitget.com", "443", pk, passphrase, sk);
+    //bg_api::ws::websockets wsp(ioctx, "ws.bitget.com", "443", pk, passphrase, sk);
 
     bg_api::rest::api api(
         ioctx,
         "api.bitget.com",
         "443",
-        pk,
-        passphrase,
-        sk,
+        "",
+        "",
+        "",
         30
+    );    
+    
+    int64_t currTime = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+    std::cout << "currTime: " << currTime << std::endl;
+    api.getServerTime(
+        [&currTime](const char *fl, int ec, std::string errmsg, bg_api::rest::server_time_t res) {
+            
+            if (ec) {
+                std::cerr << "get time error: fl=" << fl << ", ec=" << ec << ", errmsg=" << errmsg << std::endl;
+                return false;
+            }
+
+            int64_t currTime2 = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+            std::cout << "ping: " << currTime2 - currTime << std::endl;
+            std::cout << "currTime2: " << currTime2 << std::endl;
+            currTime = currTime2;
+            return true;
+        }
     );
 
+    api.getCoinList(
+        [](const char *fl, int ec, std::string errmsg, bg_api::rest::coin_list_t res) {
+            if (ec) {
+                std::cerr << "get coin list error: fl=" << fl << ", ec=" << ec << ", errmsg=" << errmsg << std::endl;
+                return false;
+            }
+
+            for (const auto &coin: res.coins) {
+                std::cout << coin << std::endl;
+            }
+            return true;
+        }
+    );
     
     ioctx.run();
 
